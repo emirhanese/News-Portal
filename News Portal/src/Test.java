e738f41c06b102a1fb09a402663a7d32c14cac64
Emirhan Ese <73592272+emirhanese@users.noreply.github.com>
2022-01-16 22:50:07 +0300

    Add files via upload

---
+++ News Portal/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    
+    public static void main(String[] args) {
+        
+        Entertainment entertainment = new Entertainment("entertainment_and_arts");
+        entertainment.printTitles();
+        System.out.println("-------------------------------------------------------------------------------------------------------");
+        entertainment.printLinks();
+        System.out.println("-------------------------------------------------------------------------------------------------------");
+        entertainment.printImages();
+    }
+}
